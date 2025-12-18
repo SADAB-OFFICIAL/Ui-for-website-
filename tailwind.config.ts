fa38e99c5c1c3fec8362d4d85f94f58d38c06d87
SADAB ALI <sadabgithub@gmail.com>
2025-12-18 17:07:46 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,12 +4,14 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", // 👈 Ye line check karo, ye honi chahiye
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // 👈 Yeh line sabse important hai styling ke liye
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         netflixRed: '#E50914',
+        netflixBlack: '#141414',
       },
     },
   },
