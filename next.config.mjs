bf68c6f1778da69ca850a213ed1cd9a8f5b8175d
SADAB ALI <sadabgithub@gmail.com>
2025-12-18 16:03:03 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,11 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "movies4u.nexus" },
-      { protocol: "https", hostname: "image.tmdb.org" },
-      { protocol: "https", hostname: "img.youtube.com" },
-      { protocol: "https", hostname: "**" }, // Allow all for scraper flexibility
+      { protocol: "https", hostname: "**" },
     ],
   },
+  // Yeh line undici error ko rokne mein madad karti hai
+  serverExternalPackages: ["cheerio"], 
 };
 
 export default nextConfig;
